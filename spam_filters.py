629502d02322e20d219e5f71c5415ec0ccf50c0f
RajeshVoorugonda007 <107318686+RajeshVoorugonda007@users.noreply.github.com>
2024-05-02 03:09:22 +0530

    commit for - showing results in single frame

--- spam_filters.py
+++ spam_filters.py
@@ -49,53 +49,36 @@ for name, clf in classifiers.items():
     model_evaluation['Precision'].append(precision_score(y_test, y_pred))
     model_evaluation['F1 Score'].append(f1_score(y_test, y_pred))
 
-
 results_df = pd.DataFrame(model_evaluation)
 
-# Plotting the table
-plt.figure(figsize=(10, 5))
-plt.title('Performance Metrics Comparison')
-plt.table(cellText=results_df.values,
-          colLabels=results_df.columns,
-          loc='center')
-plt.axis('off')
+# Plotting
+fig, ax = plt.subplots(2, 2, figsize=(14, 10))
+fig.suptitle('Performance Comparison of Spam filters')
+
+# Accuracy
+ax[0, 0].bar(results_df['Classifier'], results_df['Accuracy'], color='blue')
+ax[0, 0].set_title('Accuracy')
+ax[0, 0].set_ylim([0, 1])
+ax[0, 0].set_ylabel('Accuracy')
+
+# Recall
+ax[0, 1].bar(results_df['Classifier'], results_df['Recall'], color='green')
+ax[0, 1].set_title('Recall')
+ax[0, 1].set_ylim([0, 1])
+ax[0, 1].set_ylabel('Recall')
+
+# Precision
+ax[1, 0].bar(results_df['Classifier'], results_df['Precision'], color='red')
+ax[1, 0].set_title('Precision')
+ax[1, 0].set_ylim([0, 1])
+ax[1, 0].set_ylabel('Precision')
+
+# F1 Score
+ax[1, 1].bar(results_df['Classifier'], results_df['F1 Score'], color='purple')
+ax[1, 1].set_title('F1 Score')
+ax[1, 1].set_ylim([0, 1])
+ax[1, 1].set_ylabel('F1 Score')
+
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.show()
-
-
-plt.figure(figsize=(10, 5))
-plt.bar(results_df['Classifier'], results_df['Accuracy'], color='tan')
-plt.title('Accuracy Comparison of Spam Filters')
-plt.ylim([0, 1])
-plt.ylabel('Accuracy')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-plt.figure(figsize=(10, 5))
-plt.bar(results_df['Classifier'], results_df['Recall'], color='cyan')
-plt.title('Recall Comparison of Spam Filters')
-plt.ylim([0, 1])
-plt.ylabel('Recall')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-plt.figure(figsize=(10, 5))
-plt.bar(results_df['Classifier'], results_df['Precision'], color='lightsalmon')
-plt.title('Precision Comparison of Spam Filters')
-plt.ylim([0, 1])
-plt.ylabel('Precision')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-plt.figure(figsize=(10, 5))
-plt.bar(results_df['Classifier'], results_df['F1 Score'], color='lavender')
-plt.title('F1-score Comparison of Spam Filters')
-plt.ylim([0, 1])
-plt.ylabel('F1-score')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
 #end of code
